9ebb812745bd761e64d3558b6fcceac43ebb44fa
Varghese George <varghese_p_g@yahoo.com>
2024-12-02 18:05:17 +0530

    Commit message

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO orders (equity, quantity, order_type, price) VALUES ('AAPL', 100, 'BUY', 150.00);
+INSERT INTO orders (equity, quantity, order_type, price) VALUES ('GOOGL', 50, 'SELL', 2800.00);
